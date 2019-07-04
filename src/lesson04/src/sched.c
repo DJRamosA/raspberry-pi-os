8e20fa47131b55d6e406778025aefca13a17f64f
Diego Ramos <diegor98@gmail.com>
2019-07-04 18:02:29 -0400

    MLFQ v1

--- src/lesson04/src/sched.c
+++ src/lesson04/src/sched.c
@@ -2,9 +2,12 @@
 #include "irq.h"
 #include "printf.h"
 
+#define TIME_SLICE                  5
+#define S                          30
+
 static struct task_struct init_task = INIT_TASK;
 struct task_struct *current = &(init_task);
-struct task_struct * task[NR_TASKS] = {&(init_task), };
+struct task_struct * task[NR_TASKS*3] = {&(init_task), };
 int nr_tasks = 1;
 
 void preempt_disable(void)
@@ -17,33 +20,67 @@ void preempt_enable(void)
 	current->preempt_count--;
 }
 
+void append(int p,struct task_struct* c)
+{
+	struct task_struct * pc;
+	for(int i = NR_TASKS*p; i < NR_TASKS*(p+1); p++){
+		pc = task[i];
+		if(pc){
+			pc = c;
+		}
+	}
+}
+
 
 void _schedule(void)
 {
 	preempt_disable();
-	int next,c;
+	int next;
 	struct task_struct * p;
 	while (1) {
-		c = -1;
+		int count = 0;
 		next = 0;
-		for (int i = 0; i < NR_TASKS; i++){
-			p = task[i];
-			if (p && p->state == TASK_RUNNING && p->counter > c) {
-				c = p->counter;
-				next = i;
+		//regla 1,2 y 4
+		if(current->counter > TIME_SLICE){
+			for (int i = NR_TASKS*3; i >= 0 ; i--){
+				if (p->counter > TIME_SLICE){
+					int pri = i/NR_TASKS;
+					task[i]->counter = 0;
+					append(pri+1, task[i]);
+					task[i] == NULL;
+				}
+			}
+
+			for (int i = NR_TASKS*3; i >= 0 ; i--){
+				p = task[i];
+				if(p && p->state == TASK_RUNNING && p->counter < TIME_SLICE){
+					next = i;	
+				}
+				
+			}
+			if (next != -1){
+					switch_to(task[next]);
+					break;
 			}
 		}
-		if (c) {
+		if(current->state == TASK_RUNNING) {
+			current->counter++;
 			break;
 		}
-		for (int i = 0; i < NR_TASKS; i++) {
-			p = task[i];
-			if (p) {
-				p->counter = (p->counter >> 1) + p->priority;
+		//regla 5
+		if (count == S)
+		{
+			for (int i = NR_TASKS*3; i >= NR_TASKS ; i--){
+				int pri = i/NR_TASKS;
+				append(pri-1, task[i]);
 			}
 		}
+		//regla 3
+		
+		
+		
 	}
-	switch_to(task[next]);
+
 	preempt_enable();
 }
 
